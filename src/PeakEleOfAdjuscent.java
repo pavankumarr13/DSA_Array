ca3e04997a0469aad88b460d5c3ea9478e7c8955
pavankumarr13 <rpavankumar159@gmail.com>
2024-01-08 20:18:02 +0530

    1st program

---
+++ src/PeakEleOfAdjuscent.java
@@ -0,0 +1,27 @@
+public class PeakEleOfAdjuscent {
+    public static void getEle(int[] a) {
+        int n = a.length;
+
+        if (n == 1) {
+            System.out.println(a[0]);
+        }
+        if (a[0] >= a[1]) {
+            System.out.println(a[0]);
+        }
+        if (a[n - 1] >= a[n - 2]) {
+            System.out.println(a[n - 1]);
+        }
+        for (int i = 1; i < a.length - 1; i++) {
+            if (a[i] >= a[i - 1] && a[i] >= a[i + 1]) {
+                System.out.println(a[i]);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 50,40,30,20,12,10,90,9};
+        getEle(arr);
+
+    }
+}
