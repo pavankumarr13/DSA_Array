1981fcdd6aa60fa0525832de30f8823589a5a38d
pavankumarr13 <rpavankumar159@gmail.com>
2024-01-08 20:26:56 +0530

    Reverse array

---
+++ src/ReverseArray.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class ReverseArray {
+    public static int[] reverse(int[] a) {
+        int[] temp = new int[a.length];
+        int k = temp.length - 1;
+        for (int i = 0; i < a.length; i++) {
+            temp[k--] = a[i];
+        }
+        return temp;
+    }
+
+    public static void main(String[] args) {
+        int[] a = { 1, 2, 3, 5, 46 };
+        System.out.println(Arrays.toString(reverse(a)));
+    }
+}
